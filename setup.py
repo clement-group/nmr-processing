2690ee9d6872dd455bc3fe6d7d1d0940e3692db7
Tyler Pennebaker <pennebaker@ucsb.edu>
2024-11-25 18:50:58 -0800

    BLD: Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='nmr-processing',
+      version='0.1.0',
+      description='Tools for processing and plotting NMR data from TopSpin',
+      author='Tyler Pennebaker',
+      author_email='pennebaker@ucsb.edu',
+      packages=['nmr_processing']
+      )
